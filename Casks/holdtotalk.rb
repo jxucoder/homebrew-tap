0ef2da3ec997cb2be14c2610aa5e8cba3721bf73
J Xu <jxucoder@gmail.com>
2026-08-16 12:57:17 -0700

    Update cask metadata: product homepage, zap app support data

--- Casks/holdtotalk.rb
+++ Casks/holdtotalk.rb
@@ -5,13 +5,14 @@ cask "holdtotalk" do
   url "https://github.com/Edamame-Labs/hold-to-talk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
   name "Hold to Talk"
   desc "Free, open-source voice dictation for macOS"
-  homepage "https://github.com/Edamame-Labs/hold-to-talk"
+  homepage "https://holdtotalk.ai/"
 
   depends_on macos: ">= :sequoia"
 
   app "Hold To Talk.app"
 
   zap trash: [
+    "~/Library/Application Support/HoldToTalk",
     "~/Library/Preferences/com.holdtotalk.app.plist",
   ]
 end
