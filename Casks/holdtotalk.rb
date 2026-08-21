2596f33103f1b759f83716713e1d1798c932c6e1
J Xu <jxucoder@gmail.com>
2026-08-21 05:30:18 -0700

    Update holdtotalk to 2.3.0

--- Casks/holdtotalk.rb
+++ Casks/holdtotalk.rb
@@ -1,6 +1,6 @@
 cask "holdtotalk" do
-  version "2.2.1"
-  sha256 "d54ce853a658ec9155a3282194a29b183ac9f1a243d573da036caaecf01dacf0"
+  version "2.3.0"
+  sha256 "dd04c92f27831e5ca0cb7730b68e7f8542f9019f6e0fac2df10950e949fe98f3"
 
   url "https://github.com/Edamame-Labs/hold-to-talk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
   name "Hold to Talk"
